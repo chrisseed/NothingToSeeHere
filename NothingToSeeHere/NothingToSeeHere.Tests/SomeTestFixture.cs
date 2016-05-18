2008f04b823029f8e43aae6b128d75afd4e864fe
Chris Seed <chris@chrisseed.com>
2016-05-18 14:09:42 +0100

    Add a noddy xunit test

---
+++ NothingToSeeHere/NothingToSeeHere.Tests/SomeTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NothingToSeeHere.Tests
+{
+    public class SomeTestFixture
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            Assert.Equal(3, 1+2);
+        }
+    }
+}
